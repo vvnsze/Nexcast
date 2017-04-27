ec07f326a78cbd5470a27848fc680a6752c0a174
vvnsze <vvnsze@gmail.com>
2017-04-26 20:04:48 -0700

    [pull]retrieve itunes podcast list from server side

--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const Authentication = require('./authentication/controller');
 const passportService = require('./authentication/passport');
 const passport = require('passport');
+const Axios = require('axios');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
@@ -17,9 +18,22 @@ module.exports = function (app) {
   });
 
   app.get('/itunes', (req, res) => {
-    const searchTerm = req.query;
+    const searchTerm = req.query.term;
     console.log('this is searchTerm: ', searchTerm);
-    res.send({ cheese: 'pillow' });
+    const url = `https://itunes.apple.com/search?entity=podcast&term=${searchTerm}`;
+
+    Axios({
+      method: 'get',
+      url,
+    }).then((response) => {
+      console.log('response: ', response.data);
+      res.send({ response: response.data });
+    }).catch((e) => {
+      res.send(e);
+    });
+
+
+    // res.send({ cheese: 'pillow' });
     // function getStuff(request) {
     //
     // }
