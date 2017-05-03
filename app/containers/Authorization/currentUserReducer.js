e8781c2a306545048d209a7abee02ce97aa661f2
vvnsze <vvnsze@gmail.com>
2017-05-02 22:42:19 -0700

    [pull]fix signup

--- app/containers/Authorization/currentUserReducer.js
+++ app/containers/Authorization/currentUserReducer.js
@@ -7,7 +7,7 @@ const initialState = { authenticated: false };
 function currentUserReducer(state = initialState, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
-      return ({ user: action.user.user, authenticated: true });
+      return ({ user: action.user, authenticated: true });
     default:
       return state;
   }
